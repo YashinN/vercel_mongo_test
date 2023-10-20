994517305fdd0529a0dcde4833360372ac49013e
Yashin <yashinnaran@gmail.com>
2023-10-20 20:00:11 +0200

    switched to code 1

--- backend/index.js
+++ backend/index.js
@@ -29,63 +29,24 @@
 // module.exports = app;
 
 // index.js
-// const express = require("express");
-// const mongoose = require("mongoose");
-
-// const app = express();
-// const PORT = 4000;
-
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("Mongodb connected");
-//   })
-//   .catch((err) => {
-//     console.log({ err });
-//     process.exit(1);
-//   });
-
-// app.listen(PORT, () => {
-//   console.log(`API listening on PORT ${PORT} `);
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("Hey this is my API running 🥳");
-// });
-
-// app.get("/about", (req, res) => {
-//   res.send("This is my about route..... ");
-// });
-
-// // Export the Express API
-// module.exports = app;
-
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const app = express();
-
-const port = 4000;
-
-app.use(cors());
+const PORT = 4000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("mongoDB connected");
-    app.listen(port, () => {
-      console.log(`server running on ${port}`);
-    });
+    console.log("Mongodb connected");
   })
   .catch((err) => {
-    console.log(err);
+    console.log({ err });
     process.exit(1);
   });
 
-app.listen(port, () => {
-  console.log(`API listening on PORT ${port} `);
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
 });
 
 app.get("/", (req, res) => {
@@ -98,3 +59,43 @@ app.get("/about", (req, res) => {
 
 // Export the Express API
 module.exports = app;
+
+// .......test......
+// const express = require("express");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const app = express();
+
+// const port = 4000;
+
+// app.use(cors());
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("mongoDB connected");
+//     app.listen(port, () => {
+//       console.log(`server running on ${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     process.exit(1);
+//   });
+
+// app.listen(port, () => {
+//   console.log(`API listening on PORT ${port} `);
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("Hey this is my API running 🥳");
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("This is my about route..... ");
+// });
+
+// // Export the Express API
+// module.exports = app;
