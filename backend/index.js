3f2cd8aac82f292348b5f857c35e612c6bd09399
Yashin <yashinnaran@gmail.com>
2023-10-20 20:22:13 +0200

    added event route import.

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const app = express();
 const PORT = 4000;
 
+const eventRoutes = require("./routes/eventRoutes");
+
 app.use(cors());
 app.use("/events", eventRoutes);
 
