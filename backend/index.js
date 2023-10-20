31b53496540209d366098b88577096420ce1f88f
Yashin <yashinnaran@gmail.com>
2023-10-20 19:26:42 +0200

    new code testing.

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,63 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const app = express();
+
+// app.use(cors());
+
+// const PORT = process.env.PORT || 6000;
+
+// app.listen(PORT, () => {
+//   console.log("listening");
+// });
+
+// // mongoose
+// //   .connect(process.env.MONGO_URI)
+// //   .then(() => {
+// //     console.log("mongoDB connected");
+// //     app.listen(PORT, () => {
+// //       console.log(`server running on ${PORT}`);
+// //     });
+// //   })
+// //   .catch((err) => {
+// //     console.log(err);
+// //     process.exit(1);
+// //   });
+
+// module.exports = app;
+
+import express from "express";
+import cors from "cors";
+import http from "http";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-const PORT = process.env.PORT || 6000;
-
-app.listen(PORT, () => {
-  console.log("listening");
+app.get("/", (req, res) => {
+  res.json("HEllloooooo@@@@");
 });
 
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("mongoDB connected");
-//     app.listen(PORT, () => {
-//       console.log(`server running on ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//     process.exit(1);
-//   });
+const port = process.env.PORT || 5000;
 
-module.exports = app;
+const server = http.createServer(app);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Mongodb connected");
+    server.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log({ err });
+    process.exit(1);
+  });
