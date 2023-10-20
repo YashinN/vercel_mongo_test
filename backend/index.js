c6a5ab53c22f39110150f172819bb48611cf8f9b
Yashin <yashinnaran@gmail.com>
2023-10-20 19:22:06 +0200

    removed mongo connection

--- backend/index.js
+++ backend/index.js
@@ -9,17 +9,21 @@ app.use(cors());
 
 const PORT = process.env.PORT || 6000;
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("mongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`server running on ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
+app.listen(PORT, () => {
+  console.log("listening");
+});
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("mongoDB connected");
+//     app.listen(PORT, () => {
+//       console.log(`server running on ${PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//     process.exit(1);
+//   });
 
 module.exports = app;
