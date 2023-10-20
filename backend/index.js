93e580da7629ed027ab6a3ac3f52f3fa6fb4b58e
Yashin <yashinnaran@gmail.com>
2023-10-20 19:28:07 +0200

    Export app

--- backend/index.js
+++ backend/index.js
@@ -37,9 +37,6 @@ import "dotenv/config";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.json("HEllloooooo@@@@");
@@ -61,3 +58,5 @@ mongoose
     console.log({ err });
     process.exit(1);
   });
+
+module.exports = app;
