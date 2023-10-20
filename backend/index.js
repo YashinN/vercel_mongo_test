e309cbf6b7d59de4636a2f154c03e6e58a467baa
Yashin <yashinnaran@gmail.com>
2023-10-20 20:06:14 +0200

    dotenv moved

--- backend/index.js
+++ backend/index.js
@@ -29,10 +29,12 @@
 // module.exports = app;
 
 // index.js
-require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
+require("dotenv").config();
 
 const app = express();
 const PORT = 4000;
